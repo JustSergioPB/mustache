69dd27bc1b7ff8020ba1862cf8eba7a1e73034b5
JustSergioPB <oigresanep@gmail.com>
2023-04-18 17:27:14 +0200

    main: Hey! new palette + new auth-ui lib

--- apps/main/src/app/pages/login-page/login-page.component.ts
+++ apps/main/src/app/pages/login-page/login-page.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from '@mustache/pages';
+import { LoginComponent } from '@mustache/auth-ui';
 
 @Component({
   selector: 'main-login-page',
--- libs/auth-ui/jest.config.ts
+++ libs/auth-ui/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
-  displayName: 'pages',
+  displayName: 'auth-ui',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/libs/pages',
+  coverageDirectory: '../../coverage/libs/auth-ui',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
